92fe1b8f1f7b183ee22787c3d362f31771913196
Andrey Taranov <taranov.andrey@gmail.com>
2013-11-12 02:09:12 +0400

    Started option parsing

--- build.sbt
+++ build.sbt
@@ -3,3 +3,5 @@ name := "conscript"
 version := "0.1"
 
 scalaVersion := "2.10.3"
+
+libraryDependencies += "com.github.scopt" %% "scopt" % "2.1.0"
--- src/main/scala/conscript.scala
+++ src/main/scala/conscript.scala
@@ -1,5 +1,15 @@
+import scopt.mutable.OptionParser
+
+object Config {
+  var cleanBoot: Boolean = false
+}
+
 object Conscript {
 	def main(args: Array[String]) = {
-
+    val parser = new OptionParser("cs", "0.1") {
+      opt("clean-boot", "clears boot dir", { Config.cleanBoot = true })
+    }
+    parser.parse(args)
+    if (Config.cleanBoot) println(Config)
 	}
 }
